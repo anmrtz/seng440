ab2f83fe3f420d2443dd01596dd3ce814b4db0cf
Andres Martinez <amartinez@uvic.ca>
2021-07-21 03:23:56 -0700

    Optimize vectorized conversion

--- src/cc/cc_vector.c
+++ src/cc/cc_vector.c
@@ -75,7 +75,7 @@ static void convert_pixels(uint8_t* rgb_data, uint32_t num_pixels) {
 #else
 // Vectorized RGB->YCC pixel conversion (16 bits). Also writes converted luma pixels to YCC buffer
 static void convert_pixels(uint8_t* rgb_data, uint32_t num_pixels, uint8_t* ycc_data) {
-    uint8x8x3_t buff;
+    uint8x16x3_t buff;
 
     int16x8x3_t rgb;
     int16x8x3_t temp;
@@ -102,12 +102,18 @@ static void convert_pixels(uint8_t* rgb_data, uint32_t num_pixels, uint8_t* ycc_
     int16x8_t val_y_max = vdupq_n_s16(Y_MAX_VAL);
     int16x8_t val_c_max = vdupq_n_s16(C_MAX_VAL);
 
-    for (uint32_t pixel = 0; pixel < num_pixels; pixel += 8) {
-        buff = vld3_u8(rgb_data+pixel*3);
+    // Pre-fetch RGB pixel data
+    __asm__ __volatile__(
+    "pld\t[%0]"
+    :
+    : "r" (rgb_data));
 
-        rgb.val[0] = vreinterpretq_s16_u16(vmovl_u8(buff.val[0]));
-        rgb.val[1] = vreinterpretq_s16_u16(vmovl_u8(buff.val[1]));
-        rgb.val[2] = vreinterpretq_s16_u16(vmovl_u8(buff.val[2]));
+    for (uint32_t pixel = 0; pixel < num_pixels; pixel += 16) {
+        buff = vld3q_u8(rgb_data+pixel*3);
+
+        rgb.val[0] = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(buff.val[0])));
+        rgb.val[1] = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(buff.val[1])));
+        rgb.val[2] = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(buff.val[2])));
 
         temp.val[0] = vmulq_lane_s16(rgb.val[0], coeff1, 0);
         temp.val[1] = vmulq_lane_s16(rgb.val[0], coeff1, 3);
@@ -143,6 +149,45 @@ static void convert_pixels(uint8_t* rgb_data, uint32_t num_pixels, uint8_t* ycc_
 
         vst2_u8(rgb_data+pixel*2, c);        
         vst1_u8(ycc_data+pixel, y);
+
+        rgb.val[0] = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(buff.val[0])));
+        rgb.val[1] = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(buff.val[1])));
+        rgb.val[2] = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(buff.val[2])));
+
+        temp.val[0] = vmulq_lane_s16(rgb.val[0], coeff1, 0);
+        temp.val[1] = vmulq_lane_s16(rgb.val[0], coeff1, 3);
+        temp.val[2] = vmulq_lane_s16(rgb.val[0], coeff2, 1);
+
+        temp.val[0] = vmlaq_lane_s16(temp.val[0], rgb.val[1], coeff1, 1);
+        temp.val[1] = vmlaq_lane_s16(temp.val[1], rgb.val[1], coeff2, 0);
+        temp.val[2] = vmlaq_lane_s16(temp.val[2], rgb.val[1], coeff2, 2);
+
+        temp.val[0] = vmlaq_lane_s16(temp.val[0], rgb.val[2], coeff1, 2);
+        temp.val[1] = vmlaq_lane_s16(temp.val[1], rgb.val[2], coeff2, 1);
+        temp.val[2] = vmlaq_lane_s16(temp.val[2], rgb.val[2], coeff2, 3);
+
+        temp.val[0] = vshrq_n_s16(temp.val[0], 7);
+        temp.val[1] = vshrq_n_s16(temp.val[1], 7);
+        temp.val[2] = vshrq_n_s16(temp.val[2], 7);
+
+        temp.val[0] = vaddq_s16(temp.val[0], val_16);
+        temp.val[1] = vaddq_s16(temp.val[1], val_128);
+        temp.val[2] = vaddq_s16(temp.val[2], val_128);
+
+        temp.val[0] = vminq_s16(temp.val[0], val_y_max);
+        temp.val[1] = vminq_s16(temp.val[1], val_c_max);
+        temp.val[2] = vminq_s16(temp.val[2], val_c_max);
+
+        temp.val[0] = vmaxq_s16(temp.val[0], val_16);    
+        temp.val[1] = vmaxq_s16(temp.val[1], val_16);
+        temp.val[2] = vmaxq_s16(temp.val[2], val_16);
+
+        y = vreinterpret_u8_s8(vmovn_s16(temp.val[0]));
+        c.val[0] = vreinterpret_u8_s8(vmovn_s16(temp.val[1]));
+        c.val[1] = vreinterpret_u8_s8(vmovn_s16(temp.val[2]));
+
+        vst2_u8(rgb_data+(pixel+8)*2, c);        
+        vst1_u8(ycc_data+pixel+8, y);
     }
 }
 #endif
